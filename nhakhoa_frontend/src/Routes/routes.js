d70a969d9d38206bd28247f63e8f531bad15d28c
TranHuuTuan2003 <tranhuutuan24062003@gmail.com>
2024-09-08 22:56:01 +0700

    1

--- nhakhoa_frontend/src/Routes/routes.js
+++ nhakhoa_frontend/src/Routes/routes.js
@@ -1,11 +1,15 @@
 import config from '~/Config';
 import Home from '~/Pages/Home/Home';
 import Contact from '~/Pages/Contact/Contact';
+import DoctorList from '~/Pages/DoctorList/DoctorList';
+import DoctorDetail from '~/Pages/DoctorDetail/DoctorDetail';
 
 const publicRoutes = [
     { path: config.routes.home, component: <Home /> },
     { path: config.routes.home_1, component: <Home /> },
     { path: config.routes.contact, component: <Contact /> },
+    { path: config.routes.doctor_list, component: <DoctorList /> },
+    { path: config.routes.doctor_detail, component: <DoctorDetail /> },
 ];
 
 const privateRoutes = [];
