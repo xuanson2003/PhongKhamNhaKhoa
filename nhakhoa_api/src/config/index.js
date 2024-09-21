015373e37ce6ae0b23b06959fb5ea57bed9adb5b
cuongW123 <dosoo132003@gmail.com>
2024-09-21 23:57:28 +0700

    chia component và khởi tạo api đầu tiên

--- nhakhoa_api/src/config/index.js
+++ nhakhoa_api/src/config/index.js
@@ -1,6 +1,6 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('postgres://postgres:xuanson@localhost:5432/DentalClinic');
+const sequelize = new Sequelize('postgres://postgres:123456@localhost:5432/DentalClinic');
 
 async function connectDb() {
     try {
